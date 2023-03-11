47d6cdb61bc78f58947af741fa2a8328e54210a9
ertanenes <125890033+ertanenes@users.noreply.github.com>
2023-03-11 12:17:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+﻿const config = {
+  "botStatus": [""],
+  "prefix": ["!", "."],
+  "token": "",
+  "mongoose": "",
+  "botOwners": ["962417173043753022"],
+  "guildID": "1020455351474212954",
+  
+  "emojis": {
+    "yes_name": "yes_ramal",
+    "no_name": "no_ramal",
+    "mute_name": "ramal_mute"
+  }
+  }
+  
+  module.exports = config;
+  
